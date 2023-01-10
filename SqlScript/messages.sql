96ad4c6e5c530bddc7b6aa22dbbe9fe02c024bf7
amalia-angela <79534873+amalia-angela@users.noreply.github.com>
2023-01-10 15:32:54 +0200

    Update messages sql script.

--- SqlScript/messages.sql
+++ SqlScript/messages.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE MySqlDB;
+create DATABASE phoneserverdb;
 
-CREATE TABLE messages (
-    Id int NOT NULL AUTO_INCREMENT,
-    Sender varchar(255) NOT NULL,
-    Receiver varchar(255) NOT NULL,
-    Message varchar(255) NOT NULL,
-    Timestmp TIMESTAMP CURRENT_TIMESTAMP
+CREATE TABLE `messages` (
+  `Id` int(11) NOT NULL,
+  `Sender` varchar(255) NOT NULL,
+  `Receiver` varchar(255) NOT NULL,
+  `Message` varchar(255) NOT NULL,
+  `RegDate` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `Content` varchar(225) DEFAULT NULL
 );
-
